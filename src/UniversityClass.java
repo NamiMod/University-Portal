fb0b0ad8905dc260b440a405c2f01cfc2ee6c49f
SeyedNamiModarressi <s.namimodarressi@gmail.com>
2020-12-18 12:02:30 +0330

    university class Class Added

---
+++ src/UniversityClass.java
@@ -0,0 +1,111 @@
+/**
+ * --University Class--
+ * in this class we define classe for university
+ * @author Seyed Nami Modarressi
+ * @since 2020
+ * @version 1.0
+ */
+import java.util.ArrayList;
+
+public class UniversityClass {
+
+    private String name;
+    private int counter;
+    private int capacity;
+    private String professorName;
+    // time is array for example : time[0][0] - > shanbe 8 - 10 OR time[1][2] - > yekshanbe 14 - 16 OR ...
+    // time is 3*5 int array
+    private int[][] time;
+    private ArrayList<Student> students;
+
+    /**
+     * create new class
+     *
+     * @param name          of calss
+     * @param counter       of class
+     * @param capacity      of class
+     * @param professorName of calss
+     * @param time          time(s) of class
+     */
+    public UniversityClass(String name, int counter, int capacity, String professorName, int[][] time) {
+        this.name = name;
+        this.counter = counter;
+        this.capacity = capacity;
+        this.professorName = professorName;
+        this.time = time;
+        students = new ArrayList<Student>();
+    }
+
+    /**
+     * @return name of class
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return counter of class
+     */
+    public int getCounter() {
+        return counter;
+    }
+
+    /**
+     * @return capacity of class
+     */
+    public int getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * @return name of professor of class
+     */
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    /**
+     * @return time(s) of class
+     */
+    public int[][] getTime() {
+        return time;
+    }
+
+    public void addStudents(Student s) {
+        if (capacity>0){
+            students.add(s);
+            capacity-=1;
+        }
+    }
+
+    /**
+     * remove student from class
+     * @param s student
+     * @return is it possible or not
+     */
+    public int removeStudents(Student s) {
+        int temp=-1;
+        int i=0;
+        for (Student e : students){
+            if (e.getname().equals(s.getname())){
+                temp = i;
+                break;
+            }else {
+                i++;
+            }
+        }
+        if (temp != -1){
+            students.remove(temp);
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * get students of class
+     * @return students list
+     */
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+}
